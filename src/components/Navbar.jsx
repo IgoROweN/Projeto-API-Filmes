ce400e2ec2a7f5cf9ce2d006a6d2f457ca722270
IgoROweN <igorowen2015@hotmail.com>
2024-04-14 20:55:53 -0300

    3 Atualização para funcionamento do Deploy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
     <nav id="navbar">
       <h2>
         <Link to="/">
-          <BiCameraMovie /> MoviesLib
+          <BiCameraMovie /> Tudo Sobre Filmes
         </Link>
       </h2>
       <form onSubmit={handleSubmit}>
